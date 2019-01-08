78986cb8e057cdcb27142ad7aa1bc6d076fa7cc5
Ridha Rezzag <ridharezzag@gmail.com>
2019-01-08 00:00:47 -0500

    Responsive CSS adjestment

--- scripts/index.js
+++ scripts/index.js
@@ -22,23 +22,38 @@ fetch('http://localhost:3000/api/members')
             profileimage.setAttribute("src", element.profileImage);
             profileimage.setAttribute("alt", "Member profile Image");
 
-            // create a div element to holde the profile texts, text will holde any text in profile card like name, lastname, phone..
+            // create a div element to holde the profile infos, infos will holde any like name, lastname, phone blood type..
             var profileinformation = document.createElement("DIV");
-            profileinformation.className = "profiletext";
+            profileinformation.className = "profileinfo";
 
-            //creating a text node for name and first name
+            // create a div element to holde the profile texts, text will holde any text in profile card like name, lastname, phone..
+            var profiletexts = document.createElement("DIV");
+            profiletexts.className = "profiletext";
+
+            // create a div element to holde the profile blood type, separating blood type in a div for better designe.
+            var profilebloodtype = document.createElement("DIV");
+            profilebloodtype.className = "bloodtype";
+
+            //creating a text node for name, first name, blood type
             var memberfirtname = document.createTextNode(element.firstName);
             var memberlastName = document.createTextNode(element.lastName);
+            var memberbloodType = document.createTextNode(element.bloodType);
             var linebreak = document.createElement('br');
 
-            //appending profile name and last name to the text container
-            profileinformation.appendChild(memberfirtname);
-            profileinformation.appendChild(linebreak);
-            profileinformation.appendChild(memberlastName);
+            //appending profile name, last name, blod type to the texts container
+            profiletexts.appendChild(memberfirtname);
+            profiletexts.appendChild(linebreak);
+            profiletexts.appendChild(memberlastName);
+            
+            profileinformation.appendChild(profiletexts);
+            profileinformation.appendChild(profilebloodtype);
+            // profileinformation.appendChild(linebreak);
+            profilebloodtype.appendChild(memberbloodType);
 
             // appending profile text container and image container to the profile container
             profile.appendChild(profileimage);
             profile.appendChild(profileinformation);
+            
 
             // appending the profile built container to the dom
             document.getElementById("members").appendChild(profile);
